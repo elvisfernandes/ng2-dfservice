55715904da0b2d05a0ddd868c83b28d3a6d19e4b
Elvis Fernandes <elvis@bizconsult.com.br>
2016-11-05 08:45:20 -0200

    Providing accessors and some doc for DFResource

--- src/services/dfresource.class.ts
+++ src/services/dfresource.class.ts
@@ -1,9 +1,70 @@
+/**
+ * This class represents a resource in DreamFactory. Actually it is
+ * only a helper class to abstract URL formation.
+ * 
+ * Every DreamFactory resource is addressed following this schema:
+ * 
+ * http[s]://df_instance_url/api/v2/SERVICE_NAME/RESOURCE_TYPE/RESOURCE_NAME/RESOURCE_ID
+ */
 export class DFResource {
+    /**
+     * DreamFactory service name, as in API URL.
+     * http://yourinstance/api/v2/SERVICE_NAME
+     */
     private _serviceName:string = null;
-    private _resourceType:string = null;
-    private _resourceName:string = null;
-    private _resourceID:number = null;
+    // We won't have a setter for _serviceName, because this is a
+    // mandatory property in the constructor.
+    get serviceName():string {
+        return this._serviceName;
+    }
 
+    /**
+     * DreamFactory resource type, as in API URL.
+     * http://yourinstance/api/v2/servicename/RESOURCE_TYPE.
+     * The resource type specifies if it is a table, stored procedure, etc.
+     * The resource types are listed as static properties in DFService class. 
+     */
+    private _resourceType:string = null;
+    get resourceType():string {
+        return this._resourceType;
+    }
+    set resourceType( value:string ) {
+        // This can be changed only if not previously set up via constructor
+        if( !this._resourceType )
+            this._resourceType = value;
+    }
+
+    /**
+     * DreamFactory resource name, as in API URL.
+     * http://yourinstance/api/v2/servicename/resourcetype/RESOURCE_NAME. 
+     */
+    private _resourceName:string = null;
+    get resourceName():string {
+        return this._resourceName;
+    }
+    set resourceName( value:string ) {
+        // This can be changed only if not previously set up via constructor
+        if( !this._resourceName )
+            this._resourceName = value;
+    }
+
+    /**
+     * DreamFactory resource ID, as in API URL.
+     * http://yourinstance/api/v2/servicename/resourcetype/RESOURCE_ID.
+     */
+    private _resourceID:number = null;
+    get resourceID():number {
+        return this._resourceID;
+    }
+    set resourceID( value:number ) {
+        // This can be changed only if not previously set up via constructor
+        if( !this._resourceID )
+            this._resourceID = value;
+    }
+
+    /**
+     *  Request body, used in non-GET requests
+     */ 
     private _body:any;
     set body( body:any ) {
         this._body = body;
@@ -14,11 +75,15 @@ export class DFResource {
 
     constructor(serviceName:string, resourceType?:string, resourceName?:string, resourceID?:number) {
         if( serviceName )    this._serviceName = serviceName;
-        if( resourceType )   this._resourceType = resourceType;
-        if( resourceName )   this._resourceName = resourceName;
-        if( resourceID )     this._resourceID = resourceID;
+        if( resourceType )   this.resourceType = resourceType;
+        if( resourceName )   this.resourceName = resourceName;
+        if( resourceID )     this.resourceID = resourceID;
     }
     
+    /**
+     * Generates the resource path based on this DFResource properties,
+     * to be appended to the URL of the API endpoint. 
+     */
     getResourcePath() {
         let path = "";
         
